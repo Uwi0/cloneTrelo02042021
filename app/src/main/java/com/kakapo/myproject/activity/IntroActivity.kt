c218b161dec514ac18055554f8f3db821229d835
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-02-04 18:02:11 +0700

    sigup activity

--- app/src/main/java/com/kakapo/myproject/activity/IntroActivity.kt
+++ app/src/main/java/com/kakapo/myproject/activity/IntroActivity.kt
@@ -1,18 +1,34 @@
 package com.kakapo.myproject.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 import com.kakapo.myproject.R
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var btnSignUpIntro: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        btnSignUpIntro = findViewById(R.id.btn_sign_up_intro)
 
         window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        setSignUp()
+    }
+
+    private fun setSignUp(){
+        btnSignUpIntro.setOnClickListener{
+            val intent = Intent(this@IntroActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/kakapo/myproject/activity/SignUpActivity.kt
+++ app/src/main/java/com/kakapo/myproject/activity/SignUpActivity.kt
@@ -2,11 +2,38 @@ package com.kakapo.myproject.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
 import com.kakapo.myproject.R
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        toolbar = findViewById(R.id.toolbar_sign_up_activity)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(toolbar)
+
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_ios_24)
+        }
+
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
