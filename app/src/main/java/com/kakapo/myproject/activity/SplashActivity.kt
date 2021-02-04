77637c61e6c20905ff4e3959d09df76d412867f3
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-02-04 15:15:40 +0700

    prepare login and signup activity

--- app/src/main/java/com/kakapo/myproject/activity/SplashActivity.kt
+++ app/src/main/java/com/kakapo/myproject/activity/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.kakapo.myproject.activity
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import android.widget.TextView
 import com.kakapo.myproject.R
@@ -24,5 +26,10 @@ class SplashActivity : AppCompatActivity() {
 
         val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
         tvAppName.typeface = typeFace
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 2500)
     }
 }
