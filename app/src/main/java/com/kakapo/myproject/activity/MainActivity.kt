39f71615051d33effbd77cfd219298f47c03dc04
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-02-06 09:15:25 +0700

    set navigation bar

--- app/src/main/java/com/kakapo/myproject/activity/MainActivity.kt
+++ app/src/main/java/com/kakapo/myproject/activity/MainActivity.kt
@@ -1,12 +1,81 @@
 package com.kakapo.myproject.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.kakapo.myproject.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBar()
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
+
+    override fun onBackPressed() {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }else{
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.nav_my_profile -> {
+                Toast.makeText(
+                        this@MainActivity,
+                        "My Profile",
+                        Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            R.id.nav_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this@MainActivity, IntroActivity::class.java)
+                intent.addFlags(
+                        Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                                Intent.FLAG_ACTIVITY_NEW_TASK
+                )
+
+                startActivity(intent)
+                finish()
+            }
+
+
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(toolbar_sign_in_activity)
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        toolbar_main_activity.setOnClickListener {
+            //Toggle drawer
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer(){
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }else{
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+
 }
