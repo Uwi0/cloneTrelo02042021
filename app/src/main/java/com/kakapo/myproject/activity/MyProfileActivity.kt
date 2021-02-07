d5badf8d8792d5ea695adb9f210c5dd53e02e8af
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-02-07 08:39:25 +0700

    select image from storrage

--- app/src/main/java/com/kakapo/myproject/activity/MyProfileActivity.kt
+++ app/src/main/java/com/kakapo/myproject/activity/MyProfileActivity.kt
@@ -1,15 +1,37 @@
 package com.kakapo.myproject.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.kakapo.myproject.R
 import com.kakapo.myproject.firebase.FireStoreClass
 import com.kakapo.myproject.models.User
 import kotlinx.android.synthetic.main.activity_my_profile.*
 import kotlinx.android.synthetic.main.nav_header_main.*
+import java.io.IOException
 
-class MyProfileActivity : AppCompatActivity() {
+class MyProfileActivity : BaseActivity() {
+
+    companion object{
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mSelectedImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
@@ -17,6 +39,49 @@ class MyProfileActivity : AppCompatActivity() {
         setupActionBar()
 
         FireStoreClass().loadUserData(this)
+        setupImageFromStorage()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == READ_STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+            }else{
+                Toast.makeText(
+                    this@MyProfileActivity,
+                    "looks like you just denied the permissions," +
+                            " you can change in android setting UwU",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(
+            resultCode == Activity.RESULT_OK &&
+            requestCode == PICK_IMAGE_REQUEST_CODE &&
+            data!!.data != null
+        ){
+            mSelectedImageUri = data.data
+
+            try{
+                Glide
+                     .with(this@MyProfileActivity)
+                     .load(mSelectedImageUri)
+                     .centerCrop()
+                     .placeholder(R.drawable.ic_user_place_holder)
+                     .into(iv_profile_user_image)
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun setupActionBar(){
@@ -35,18 +100,73 @@ class MyProfileActivity : AppCompatActivity() {
     }
 
     fun setUserDataInUi(user: User) {
+        showProgressDialog(resources.getString(R.string.please_wait))
         Glide
             .with(this@MyProfileActivity)
             .load(user.image)
             .centerCrop()
             .placeholder(R.drawable.ic_user_place_holder)
-            .into(iv_user_image)
+            .listener(object : RequestListener<Drawable>{
+                override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                ): Boolean {
+                    hideProgressDialog()
+                    return false
+                }
+
+                override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                ): Boolean {
+                    hideProgressDialog()
+                    return false
+                }
+
+            })
+            .into(iv_profile_user_image)
 
         et_name.setText(user.name)
         et_email.setText(user.email)
         if(user.mobileNumber != 0L){
             et_mobile.setText(user.mobileNumber.toString())
         }
+    }
+
+    private fun setupImageFromStorage(){
+        iv_profile_user_image.setOnClickListener {
+
+            if(ContextCompat.checkSelfPermission(
+                    this@MyProfileActivity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ){
+
+                showImageChooser()
+
+            }else{
+                ActivityCompat.requestPermissions(
+                    this@MyProfileActivity,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+
+        }
+    }
+
+    private fun showImageChooser(){
+        val galleryIntent = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
     }
 
 }
